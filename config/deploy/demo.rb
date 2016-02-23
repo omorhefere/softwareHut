8f555eba21c962acced3a7eccc8c7ae95fd6d497
Vlad Balanescu <vbalanescu1@sheffield.ac.uk>
2016-02-23 11:43:55 +0000

    Fixed encoding issue

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,6 +1,6 @@
 ## Application deployment configuration
 set :server,      'epi-stu-hut-demo1.shef.ac.uk'
-set :user,        'demo.team06​'
+set :user,        'demo.team06'
 set :deploy_to,   -> { "/srv/services/#{fetch(:user)}" }
 set :log_level,   :debug
 
