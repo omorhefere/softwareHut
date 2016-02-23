d95885b9e07bfd51d2a5552bea7d92c5ee1ef253
Vlad Balanescu <vbalanescu1@sheffield.ac.uk>
2016-02-23 11:45:27 +0000

    Updated project git address

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 ## Application configuration
 set :application,             'team6'
 set :scm,                     :git
-set :repo_url,                'git@git.software-hut.org.uk:team06/project.git'
+set :repo_url,                'git@git.software-hut.org.uk:hut-team06/project.git'
 set :linked_files,            fetch(:linked_files,  fetch(:env_links, [])).push('config/database.yml', 'config/secrets.yml')
 set :linked_dirs,             fetch(:linked_dirs, []).push('log', 'tmp/pids', 'uploads')
 # set the locations to look for changed assets to determine whether to precompile
