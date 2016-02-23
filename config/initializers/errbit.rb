ede44c49f4ffce0994b9cba99dbbd9afeb2812e5
Vlad Balanescu <vbalanescu1@sheffield.ac.uk>
2016-02-23 11:58:55 +0000

    fix errbit config

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,6 @@
-Airbrake.configuredo|config|
-
-config.api_key='​78bd69b8fbd8a8900f9b7fca40bcc0b0'
-config.host ='errbit.software‐hut.org.uk'
-config.port =443
-config.secure =config.port==443
+Airbrake.configure do |config|
+  config.api_key='​78bd69b8fbd8a8900f9b7fca40bcc0b0'
+  config.host ='errbit.software‐hut.org.uk'
+  config.port =443
+  config.secure =config.port==443
 end
