dec22470d97b4bc4743f373ec1d9e1ec181fb6e3
aca14mkk <mkkulatunga1@sheffield.ac.uk>
2016-02-26 12:39:23 +0000

    Created empty  projects Controller, generated routes to view/show/edit/new project. Created views file show.html.haml for projects (localhost:3000/projects/show) and added basic html to it.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   root to: "pages#home"
 
+
+  resources :projects
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
