5b0c84009219bdb3be8280cf84b382e914866b03
Vlad Balanescu <vbalanescu1@sheffield.ac.uk>
2016-03-08 20:17:03 +0000

    Flash message added when logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,41 +6,50 @@ class PagesController < ApplicationController
 
   def add_project
     @current_nav_identifier = :add_new_project
+    $user_logged_in = false
   end
 
   def log_in_out
     @current_nav_identifier = :log_in_out
     @disabled_nav = true
     @sticky_footer=true
+    $user_logged_in = true
   end
 
   def zero_failures
     @current_nav_identifier = :home
+    $user_logged_in = false
   end
 
   def rre
     @current_nav_identifier = :home
+    $user_logged_in = false
   end
 
   def bip
     @current_nav_identifier = :home
+    $user_logged_in = false
   end
 
   def show
+    $user_logged_in = false
   end
 
   def contact
     @current_nav_identifier = :contact
     @sticky_footer=true
+    $user_logged_in = false
   end
 
   def forgot_password
     @disabled_nav = true
     @sticky_footer=true
+    $user_logged_in = false
   end
 
   def admin
     @sticky_footer=true
     @current_nav_identifier = :admin
+    $user_logged_in = false
   end
 end
