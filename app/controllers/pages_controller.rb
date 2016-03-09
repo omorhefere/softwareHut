a268191dc89f8e4ee05a28dda3c823712e7c12fc
Vlad Balanescu <vbalanescu1@sheffield.ac.uk>
2016-03-09 10:40:42 +0000

    Flash message edited

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,57 +6,57 @@ class PagesController < ApplicationController
 
   def add_project
     @current_nav_identifier = :add_new_project
-    $user_logged_in = false
+    $logged_in_message = false
   end
 
   def log_in_out
     @current_nav_identifier = :log_in_out
     @disabled_nav = true
     @sticky_footer=true
-    $user_logged_in = true
+    $logged_in_message = true
   end
 
   def zero_failures
     @current_nav_identifier = :home
-    $user_logged_in = false
+    $logged_in_message = false
   end
 
   def rre
     @current_nav_identifier = :home
-    $user_logged_in = false
+    $logged_in_message = false
   end
 
   def bip
     @current_nav_identifier = :home
-    $user_logged_in = false
+    $logged_in_message = false
   end
 
   def show
-    $user_logged_in = false
+    $logged_in_message = false
   end
 
   def contact
     @current_nav_identifier = :contact
     @sticky_footer=true
-    $user_logged_in = false
+    $logged_in_message = false
   end
 
   def forgot_password
     @disabled_nav = true
     @sticky_footer=true
-    $user_logged_in = false
+    $logged_in_message = false
+  end
+
+  def admin
+    @sticky_footer=true
+    @current_nav_identifier = :admin
+    $logged_in_message = false
   end
 
   def registration
     @current_nav_identifier = :registration
     @disabled_nav = true
     @sticky_footer = true
-  end
-
-  def admin
-    @sticky_footer=true
-    @current_nav_identifier = :admin
-    $user_logged_in = false
   end
 
   def new_password
