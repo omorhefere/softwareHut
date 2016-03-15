7a884478a0f6e25d1bc49da01579b807504562ce
aca14mkk <mkkulatunga1@sheffield.ac.uk>
2016-03-15 12:04:52 +0000

    Created table projects. Added basic css for projects new and projects show pages

--- db/migrate/20160311142300_create_users.rb
+++ db/migrate/20160311142300_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-      t.string :confirm_password, null: false, default: ""
+      t.string :confirm_password,   null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
