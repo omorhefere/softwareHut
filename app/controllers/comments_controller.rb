e4a6b6c75e263deaaf6f8156025c118fae290f55
aca14mkk <mkkulatunga1@sheffield.ac.uk>
2016-04-11 12:44:08 +0100

    Options to delete projects and comments from project show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
     redirect_to project_path(@project)
   end
 
+  def destroy
+    @project = Project.find params[:project_id]
+    @comment = @project.comments.find params[:id]
+    @comment.destroy
+    redirect_to project_path(@project)
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:commenter, :body)
