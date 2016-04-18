d7418035c3dac479831295eb80ac6825b145a8e5
Simou Wang <swang14@sheffield.ac.uk>
2016-04-18 12:06:03 +0100

    added veridation field

--- db/schema.rb
+++ db/schema.rb
@@ -85,7 +85,6 @@ ActiveRecord::Schema.define(version: 20160411120119) do
     t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
-    t.string   "password_confirmation",  default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,6 @@
 #  name                   :string
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  password_confirmation  :string           default(""), not null
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
