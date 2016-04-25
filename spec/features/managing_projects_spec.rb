b3b3ca203e1ecab07e0f12687fa6c6531c8c4e41
Vlad Balanescu <vbalanescu1@sheffield.ac.uk>
2016-04-25 15:41:43 +0100

    Integration testing added here (7ok, 1failed)

--- spec/features/managing_projects_spec.rb
+++ spec/features/managing_projects_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 describe 'Log in' do
   specify 'I can login' do
-    visit '/users/sign_in'
-    fill_in 'Email', with: 'andreea@gmail.com'
-    fill_in 'Password', with: '12341234'
-    click_button 'Login'
-    expect(page).to have_content('Signed in successfully.')
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit '/home'
+    expect(page).to have_content('All projects')
   end
 end
 
@@ -15,5 +14,60 @@ describe 'Forgot password' do
     visit '/users/sign_in'
     click_button 'Forgot Password'
     expect(page).to have_content('Reset Password')
+  end
+end
+
+describe 'Search' do
+  specify 'Search a project' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit '/home'
+    fill_in 'Search Projects', with: 'some project title'
+    click_button 'SearchButton'
+    expect(page).to have_content('Search results')
+  end
+end
+
+describe 'Categories' do
+  specify 'Pick category - All projects' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit '/home'
+    click_link 'Categories'
+    click_link 'All projects'
+    expect(page).to have_content('All projects')
+  end
+end
+
+describe 'Categories' do
+  specify 'Pick category - Zero Failures' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit '/home'
+    click_link 'Categories'
+    click_link 'Zero Failures'
+    expect(page).to have_content('Zero Failures')
+  end
+end
+
+describe 'Categories' do
+  specify 'Pick category - Resource, Recovery and Efficiency' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit '/home'
+    click_link 'Categories'
+    click_link 'Resource, Recovery and Efficiency'
+    expect(page).to have_content('Resource, Recovery and Efficiency')
+  end
+end
+
+describe 'Categories' do
+  specify 'Pick category - Buried Infrastructure Performance' do
+    user = FactoryGirl.create(:user)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit '/home'
+    click_link 'Categories'
+    click_link 'Buried Infrastructure Performance'
+    expect(page).to have_content('Buried Infrastructure Performance')
   end
 end
