39a360ca85c59969f9509b40f07e7c93932f6af0
aca14mkk <mkkulatunga1@sheffield.ac.uk>
2016-04-26 13:03:23 +0100

    removed unwanted 'image url field' from add project page

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -1,3 +1,44 @@
+# == Schema Information
+#
+# Table name: projects
+#
+#  id                  :integer          not null, primary key
+#  project_number      :string
+#  priority_duration   :string
+#  project_title       :string
+#  aims                :string
+#  why_important       :string
+#  would_do_project    :string
+#  funding             :string
+#  time_scale          :string
+#  benifits            :string
+#  methodology         :string
+#  stage               :string
+#  volunteers          :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  category_id         :integer
+#  subcategory_id      :integer
+#  image_remote_url    :string
+#  user_id             :integer
+#  image1_file_name    :string
+#  image1_content_type :string
+#  image1_file_size    :integer
+#  image1_updated_at   :datetime
+#  image2_file_name    :string
+#  image2_content_type :string
+#  image2_file_size    :integer
+#  image2_updated_at   :datetime
+#  image3_file_name    :string
+#  image3_content_type :string
+#  image3_file_size    :integer
+#  image3_updated_at   :datetime
+#  image4_file_name    :string
+#  image4_content_type :string
+#  image4_file_size    :integer
+#  image4_updated_at   :datetime
+#
+
 FactoryGirl.define do
   factory :project do
     project_number       "1"
--- spec/factories/subcategories.rb
+++ spec/factories/subcategories.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: subcategories
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :subcategory do
     name        "Knwoledge"
