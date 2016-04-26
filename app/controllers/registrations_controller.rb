9336d5d51834de47bb070e9386e5b7f4c71ce80a
omorhefere <ooimoloame1@sheffield.ac.uk>
2016-04-26 20:46:37 +0100

    Sends Welcome Email

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -15,6 +15,7 @@ class RegistrationsController < Devise::RegistrationsController
     @user.valid?
     if @user.errors.blank?
       @user.save
+      welcome_email(user)
       redirect_to :new_user_session
     else
       render :action => "new"
