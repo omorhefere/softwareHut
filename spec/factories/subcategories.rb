db69ccd94b83f76f1992fcb518b173665ce6ab20
Vlad Balanescu <vbalanescu1@sheffield.ac.uk>
2016-04-26 23:39:45 +0100

    subcategories mistake solved

--- spec/factories/subcategories.rb
+++ spec/factories/subcategories.rb
@@ -10,11 +10,7 @@
 
 FactoryGirl.define do
   factory :subcategory do
-<<<<<<< HEAD
     name        "Knowledge"
-=======
-    name        "Economic"
->>>>>>> 1668b2f692438c64bbc3fde9beac5edc73f034c6
     created_at  "02/04/2015"
     updated_at  "02/04/2015"
   end
