357649858471ce76600ecda96b44f57da0d37dd3
aca14mkk <mkkulatunga1@sheffield.ac.uk>
2016-04-27 10:17:49 +0100

    Deleted unwatned migrations

--- db/migrate/20160427085527_remove_benifits_from_projects.rb
+++
@@ -1,6 +0,0 @@
-class RemoveBenifitsFromProjects < ActiveRecord::Migration
-  def change
-    remove_column :projects, :benifits, :string
-    remove_column :projects, :, :string
-  end
-end
--- db/migrate/20160427085549_add_benefits_from_projects.rb
+++
@@ -1,6 +0,0 @@
-class AddBenefitsFromProjects < ActiveRecord::Migration
-  def change
-    add_column :projects, :benefits, :string
-    add_column :projects, :, :string
-  end
-end
