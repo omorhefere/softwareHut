549c0af1f9007ed36b86b5ceb7c5cc1f66e4306a
Vlad Balanescu <vbalanescu1@sheffield.ac.uk>
2016-04-28 16:00:34 +0100

    Deleted unused files, all integration tests in one file now

--- spec/features/full_app_spec.rb
+++ spec/features/full_app_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+#MANAGING Projects#
 # Log in page tests
 describe 'Log in as user' do
   specify 'I can login' do
@@ -293,5 +294,57 @@ describe 'Contact' do
     fill_in 'Write Your Message Here', with: 'Message'
     click_button 'Send message'
     expect(page).to have_content('Cannot send message')
+  end
+end
+
+#MANAGING USERS#
+# Admin area tests
+describe 'Navigation - categories' do
+  specify 'I can see all the categories' do
+    user = FactoryGirl.create(:user, admin: true)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit '/admin'
+    click_link 'Categories'
+    expect(page).to have_content('List of Categories')
+  end
+end
+
+describe 'Navigation - Comments' do
+  specify 'I can see all the comments' do
+    user = FactoryGirl.create(:user, admin: true)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit '/admin'
+    click_link 'Comments'
+    expect(page).to have_content('List of Comments')
+  end
+end
+
+describe 'Navigation - Projects' do
+  specify 'I can see all the projects' do
+    user = FactoryGirl.create(:user, admin: true)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit '/admin'
+    click_link 'Projects'
+    expect(page).to have_content('List of Projects')
+  end
+end
+
+describe 'Navigation - Subcategories' do
+  specify 'I can see all the subcategories' do
+    user = FactoryGirl.create(:user, admin: true)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit '/admin'
+    click_link 'Subcategories'
+    expect(page).to have_content('List of Subcategories')
+  end
+end
+
+describe 'Navigation - Users' do
+  specify 'I can see all the users' do
+    user = FactoryGirl.create(:user, admin: true)
+    login_as(user, :scope => :user, :run_callbacks => false)
+    visit '/admin'
+    click_link 'Users'
+    expect(page).to have_content('List of Users')
   end
 end
