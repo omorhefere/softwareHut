33468b1c44a2f235c3d35d5ed6effac09c1ffe4f
omorhefere <ooimoloame1@sheffield.ac.uk>
2016-05-03 17:42:02 +0100

    Working Contact Form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,17 +1,22 @@
 class ContactsController < ApplicationController
 
-#  def new
-#    @contact = Contact.new
-#  end
+  def new
+   @contact = Contact.new
+  end
 
-#  def create
-#    @contact = Contact.new(params[:contact])
-#    @contact.request = request
-#    if @contact.deliver
-#      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
-#    else
-#      flash.now[:error] = 'Cannot send message'
-#      render :new
-#    end
-#  end
+  def create
+    @contact = Contact.new(contact_params.merge(email: current_user.email, name: current_user.name))
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:notice] = 'Thank you for your message. We will contact you soon'
+    else
+      flash.now[:error] = 'Cannot send message'
+      render :new
+    end
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :subject, :message,:nickname)
+    end
 end
