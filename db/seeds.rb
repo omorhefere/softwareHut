656d67a496f53f9e70c2d455b357a0812dbf52d5
aca14mkk <mkkulatunga1@sheffield.ac.uk>
2016-05-09 20:57:15 +0100

    Seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-User.where(email: 'test@sheffield.ac.uk').first_or_create(password:'12341234000')
+
+#Default admin account
+User.where(name: 'Admin', email: 'admin@sheffield.ac.uk', approved: 't', admin: 't').first_or_create(password:'12341234')
+
+#Default categories
+Category.create(name: 'Zero Failures')
+Category.create(name: 'Resource Recovery and Efficiency')
+Category.create(name: 'Buried Infrastructure Performance')
+
+Subcategory.create(name: 'Economic')
+Subcategory.create(name: 'Technology')
+Subcategory.create(name: 'Knowledge')
