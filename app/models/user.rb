61757c6918393791e304a89de604dfcf872e782a
omorhefere <ooimoloame1@sheffield.ac.uk>
2016-05-09 13:06:52 +0100

    uniqueness fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -52,6 +52,7 @@ class User < ActiveRecord::Base
   has_many :projects
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/avatar.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates_uniqueness_of :invitation_token, :case_sensitive => false
 
   def active_for_authentication?
    super && approved?
