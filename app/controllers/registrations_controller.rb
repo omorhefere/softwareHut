f5ffb16f3fd4bb8c10c5d90ac771b0383d2a11da
aca14mkk <mkkulatunga1@sheffield.ac.uk>
2016-05-12 16:59:34 +0100

    Removed redundant code

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,25 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
    before_filter :configure_permitted_parameters
 
-  def new
-    @user= User.new
-  end
-
-  def create
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation =params[:user][:password_confirmation]
-
-    @user.valid?
-    if @user.errors.blank?
-      @user.save
-      redirect_to :new_user_session
-    else
-      render :action => "new"
-    end
-  end
 
   protected
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,48 +19,13 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/1
-  def show
-    @current_nav_identifier = :registration
-    @disabled_nav = true
-    @sticky_footer = true
-  end
-
-  # GET /users/1/edit
-  def edit
-    @current_nav_identifier = :registration
-    @disabled_nav = true
-    @sticky_footer = true
-  end
 
   # POST /users
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to @user, notice: 'User was successfully created.'
-    else
-      render :new
-    end
-  end
+
 
   # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      redirect_to :profile, notice: 'User was successfully updated.'
-    else
-      render :edit
-    end
-    @current_nav_identifier = :registration
-    @disabled_nav = true
-    @sticky_footer = true
-  end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
-  end
-
 
   private
     # Use callbacks to share common setup or constraints between actions.
