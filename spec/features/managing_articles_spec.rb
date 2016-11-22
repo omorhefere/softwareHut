7b5254b75424100883dec37cab316e0a501151a0
omorhefere <ooimoloame1@sheffield.ac.uk>
2016-11-22 21:58:22 +0000

    finished

--- spec/features/managing_articles_spec.rb
+++ spec/features/managing_articles_spec.rb
@@ -3,11 +3,8 @@ require 'rails_helper'
 #MANAGING Articles#
 describe 'Show an article' do
   specify 'I can click on READ MORE to view an article' do
-    user = FactoryGirl.create(:user, approved: true)
     FactoryGirl.create(:article)
-    login_as(user, :scope => :user, :run_callbacks => false)
     visit '/home'
-    click_link 'News'
     click_link 'Read More'
     expect(page).to have_no_content('Read More')
   end
